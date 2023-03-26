7887197b74b73d551aa598604cbcdff7cc43ec60
akshat1920 <60009548+akshatranadive@users.noreply.github.com>
2023-03-25 21:13:32 -0400

    updated flight data

--- router/flights.js
+++ router/flights.js
@@ -48,19 +48,19 @@ router.get("/flights", async (req, res) => {
   // console.log(data);
 
   data.forEach((element) => {
-    //element.forEach((obj) => {
-    //   let match = 0;
-    //   queryAmenities.forEach((amenity) => {
-    //     if (obj.ammenities.includes(amenity)) {
-    //       match++;
-    //     }
-    //   });
+    element.forEach((obj) => {
+      // let match = 0;
+      // queryAmenities.forEach((amenity) => {
+      //   if (obj.ammenities.includes(amenity)) {
+      //     match++;
+      //   }
+      // });
 
-    //   if (match == queryAmenities.length) {
-    //     finalData.push(obj);
-    //   }
-    // });
-    finalData.push(element);
+      // if (match == queryAmenities.length) {
+      //   finalData.push(obj);
+      // }
+      finalData.push(obj);
+    });
   });
 
   res.send(finalData);
